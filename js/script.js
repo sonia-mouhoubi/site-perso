a8e83cb6e2bb6f97d339bb0d855b148f93232c9a
M <sonia.mouhoubi@laplateforme.io>
2022-04-04 11:27:46 +0200

    ProjectPool2 Portfolio (Site perso)

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", () => {
+    // let lienNav = document.querySelector('nav a'),
+    //     nav = document.querySelector('nav'), 
+    //     buttonChildOne = document.querySelector('.menu-burger :nth-child(1)'),
+    //     buttonChildTwo = document.querySelector('.menu-burger :nth-child(2)');
+
+        
+    //     lienNav.addEventListener('click', function () {
+    //         nav.style.display = "none";
+    // });
+});
